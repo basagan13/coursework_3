8b0c919e7a5114bfb774dcd70cd504581caa8cf7
Katya <basagan13@gmail.com>
2023-08-03 23:07:13 +0300

    adding comments

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+# Импорты
 import os
 from src.utils import load_data_from_json,\
     make_list_of_class_objects, sort_list, print_details
 
+# Источник информации, json-файл
 SOURCE = os.path.join('', 'data', 'operations.json')
 
+# Получаем данные из json-файла, создаем список экземпляров класса
 raw_data = load_data_from_json(SOURCE)
 operations_executed = make_list_of_class_objects(raw_data)
+
+# Сортируем список, берем наиболее поздние 5 операций
 operations_sorted = sort_list(operations_executed)
 last_5_operations = operations_sorted[:5]
 
+# Вывод информации на экран
 for operation in last_5_operations:
     print(operation.print_1st_line())
     print_details(operation)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,13 +1,18 @@
 import datetime
 from src.utils import load_data_from_json, make_list_of_class_objects, sort_list
-from src.class_operation import Operation
 
 
 def test_load_data_from_json(json_file):
+    '''
+    Проверяем, полуилось ли загрузить инорфмацию из json-файла
+    '''
     assert type(load_data_from_json(json_file)) == list
 
 
 def test_make_list_of_class_objects(data_for_test):
+    '''
+    Проверяем тип - список, у 3 объектов - status(EXECUTED), разные данные
+    '''
     assert type(make_list_of_class_objects(data_for_test)) == list
     assert len(make_list_of_class_objects(data_for_test)) == 3
     assert make_list_of_class_objects(data_for_test)[0].from_ == 'from1'
@@ -17,6 +22,9 @@ def test_make_list_of_class_objects(data_for_test):
 
 
 def test_sort_list(list_for_test):
+    '''
+    Проверяем тип - список, 4 объекта, верный порядок
+    '''
     assert type(sort_list(list_for_test)) == list
     assert len(sort_list(list_for_test)) == 4
     assert sort_list(list_for_test)[0].descr == 'descr2'
